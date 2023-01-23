b4e605941c07e580ed69417bb228c2bff483f350
hadi <hadii.tanoomand@gmail.com>
2023-01-24 01:35:45 +0330

    add_loading_skeleton

--- get-users/src/components/users.jsx
+++ get-users/src/components/users.jsx
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import LoadingUsers from './loading/loadingUsers';
 
 class Users extends Component {
   state = { 
-    users : []
+    users : [],
+    isLoading : true
    } 
 
    async componentDidMount(){
     const response =await axios.get('https://reqres.in/api/users');
-    this.setState({users:response.data.data})
+    setTimeout(() => {
+      this.setState({users:response.data.data , isLoading:false})
+    }, 3000);
    }
 
   render() { 
@@ -16,28 +20,34 @@ class Users extends Component {
     <>
     <button onClick={this.handleCreate} className='btn btn-lg btn-primary'>Create</button>
       <div className='row' >
-        {
-          this.state.users.map((user)=>{
-            return(
-              <div className='col-4 text-center p-5'>
-                <img src={user.avatar} style={{borderRadius:'50%' , width: '100px'}} />
-                <h4>{user.first_name}{user.last_name}</h4>
-                <h5>{user.email}</h5>
-                <div className='row'>
-                  <div className="col-6">
-                  <button onClick={this.handleDelete} className='btn btn-sm btn-danger'>delete</button>
+       {
+        this.state.isLoading ? (
+          <LoadingUsers />
+        ) : (
+          
+            this.state.users.map((user)=>{
+              return(
+                <div className='col-4 text-center p-5'>
+                  <img src={user.avatar} style={{borderRadius:'50%' , width: '100px'}} />
+                  <h4>{user.first_name}{user.last_name}</h4>
+                  <h5>{user.email}</h5>
+                  <div className='row'>
+                    <div className="col-6">
+                    <button onClick={this.handleDelete} className='btn btn-sm btn-danger'>delete</button>
+                    </div>
+                    <div className="col-6">
+                    <button onClick={this.handleUpdate} className='btn btn-sm btn-primary'>update</button>
+                    </div>
+  
                   </div>
-                  <div className="col-6">
-                  <button onClick={this.handleUpdate} className='btn btn-sm btn-primary'>update</button>
-                  </div>
-
                 </div>
-              </div>
-            )  
-         
-
-          })
-        }
+              )  
+           
+  
+            })
+          
+        )
+       }
        
       </div>
     </>);
